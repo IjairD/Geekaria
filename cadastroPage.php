77d7f373b41a9c3260ec565a5a810f659b6df80f
Vettori892 <vettori892@gmail.com>
2025-07-06 21:30:13 -0300

    Update cadastroPage.php

--- cadastroPage.php
+++ cadastroPage.php
@@ -18,23 +18,25 @@
                 <h1><span>Cadas</span>tro</h1>
                 <form action="cadastro.php" method="post">
                     <label for="name">Nome</label>
-                    <input type="text" id="name" name="name" spellcheck="false" require autofocus><br>
+                    <input type="text" id="name" name="name" spellcheck="false" required autofocus><br>
                     <label for="celular">Celular</label>
-                    <input type="text" id="celular" name="celular"><br>
+                    <input type="text" id="celular" name="celular" placeholder="(00) 00000-0000" required><br>
                     <label for="user">Usuario</label>
-                    <input type="text" id="user" name="user"><br>
+                    <input type="text" id="user" name="user" required><br>
                     <label for="password">Senha</label>
-                    <input type="password" id="password" name="password" require><br>
+                    <input type="password" id="password" name="password" required><br>
                     <label for="password2">Confirmar Senha</label>
-                    <input type="password" id="password2" name="password2" require>
+                    <input type="password" id="password2" name="password2" required>
                     <div class="trigger">
                         <button type="button" class="button" onclick="window.location.href='loginPage.php'">Cancelar</button>
                         <button type="submit" class="button">Criar conta</button>
                     </div>
+                    <p id="erroSenha" style="color:red; display:none;">Senhas não correspondem</p>
                 </form>
             </div>
         </div>
     </main>
     <?php require 'geral/rodape.php'?>
+    <script src="JS/script.js"></script>
 </body>
 </html>
